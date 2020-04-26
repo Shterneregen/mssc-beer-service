920b43df00aae0acfa2a4847a55e1277ec5f18d2
shterneregen <shterneregen@gmail.com>
2020-04-26 04:13:59 +0300

    cleaning up build.gradle & fixing mapper

--- src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper(uses = DateMapper.class)
+@Mapper(componentModel = "spring", uses = {DateMapper.class})
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
