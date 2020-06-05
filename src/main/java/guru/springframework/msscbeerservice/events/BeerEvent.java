231725bad565e5dcefaa1b14eddb51c2f71d78bc
shterneregen <shterneregen@gmail.com>
2020-06-06 01:17:14 +0300

    fixing Jackson config

--- src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -1,18 +1,20 @@
 package guru.springframework.msscbeerservice.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @Builder
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class BeerEvent implements Serializable {
 
     static final long serialVersionUID = -3429088061263553575L;
 
-    private final BeerDto beerDto;
+    private BeerDto beerDto;
 }
